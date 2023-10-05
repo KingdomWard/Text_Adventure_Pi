72c366c53ba95ab1ef59d740fe20d0df3359f5d3
John <119105460+KingdomWard@users.noreply.github.com>
2023-10-05 04:06:40 -0400

    Update mainmenu.py
    main menu, can press enter or quit. displays title.

--- mainmenu.py
+++ mainmenu.py
@@ -1,22 +1,51 @@
 import pygame
+import sys
 
+# start game
 pygame.init()
 
-# displaying window
-pygame.display.set_mode((1200, 800))
-  
-# creating a bool value which checks
-# if game is running
-running = True
-  
-# keep game running till running is true
-while running:
-      
-    # Check for event if user has pushed
-    # any event in queue
-    for event in pygame.event.get():
-          
-        # if event is of type quit then 
-        # set running bool to false
-        if event.type == pygame.QUIT:
-            running = False
+# declared variables for screen size, color, font
+SCREEN_WIDTH = 800
+SCREEN_HEIGHT = 600
+
+WHITE = (255, 255, 255)
+BLACK = (0, 0, 0)
+
+FONT = pygame.font.Font(None, 36)
+
+# create the game window
+screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+pygame.display.set_caption("Text Adventure Game")
+
+# draw the text of the menu
+def draw_text(text, font, color, x, y):
+    text_surface = font.render(text, True, color)
+    text_rect = text_surface.get_rect()
+    text_rect.center = (x, y)
+    screen.blit(text_surface, text_rect)
+
+# while loop that keeps game running unless quit or press ESC
+# If Enter is pressed, will start the main game.
+def main_menu():
+    while True:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                sys.exit()
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_RETURN:
+                    return "start"
+                if event.key == pygame.K_ESCAPE:
+                    pygame.quit()
+                    sys.exit()
+
+        screen.fill(BLACK)
+
+        draw_text("Text Adventure Pi", FONT, WHITE, SCREEN_WIDTH // 2, SCREEN_HEIGHT // 4)
+        draw_text("Press ENTER to Start!", FONT, WHITE, SCREEN_WIDTH // 2, SCREEN_HEIGHT // 2)
+        draw_text("Press ESC to Quit", FONT, WHITE, SCREEN_WIDTH // 2, SCREEN_HEIGHT // 2 + 50)
+
+        pygame.display.flip()
+
+if __name__ == "__main__":
+    main_menu()
